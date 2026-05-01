e1d65dd13ee11d66055efcee867ab1a273d81c48
RyotaroHiraki <126726402+RyotaroHiraki@users.noreply.github.com>
2026-05-01 14:06:11 +0000

    Deploying to gh-pages from @ RyotaroHiraki/RyotaroHiraki.github.io@9dc191e8d4b5e7014dac2ee2f6c4f629c8e322b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -124,7 +124,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%68%69%72%61%6B%69@%75%61%72%6B.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
